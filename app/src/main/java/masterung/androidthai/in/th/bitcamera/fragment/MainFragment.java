71c38c07f49474fc4f9b1c9e8b7e9ff0f6025bf4
masterUNG <phrombutr@gmail.com>
2018-08-17 11:37:27 +0700

    QR Camera OK

--- app/src/main/java/masterung/androidthai/in/th/bitcamera/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/bitcamera/fragment/MainFragment.java
@@ -9,8 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import masterung.androidthai.in.th.bitcamera.R;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create QRscanner
+        ZXingScannerView zXingScannerView = getView().findViewById(R.id.zxingMain);
+        zXingScannerView.startCamera();
+
+    }   // Main Method
 
     @Nullable
     @Override
