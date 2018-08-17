8418ce91099603ae1d6222f74094b14c9afb056f
masterUNG <phrombutr@gmail.com>
2018-08-17 11:01:06 +0700

    Add Fragment

--- app/src/main/java/masterung/androidthai/in/th/bitcamera/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/bitcamera/MainActivity.java
@@ -3,11 +3,24 @@ package masterung.androidthai.in.th.bitcamera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.bitcamera.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
